19e009f6d12286abb67f4347bae2b9b45d86eecc
Jean B <jean.barros.teixeira@gmail.com>
2020-08-15 19:49:33 -0300

    Add Post model

--- meu_blog/blog/models.py
+++ meu_blog/blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+
+    STATUS = (
+        ('rascunho','Rascunho'),
+        ('publicado','Publicado')
+    )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE)
+    content = models.TextField()
+    published = models.DateTimeField(default=timezone.now())
+    created = models.DateTimeField(auto_now_add=True)
+    altered = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10,
+                              choices=STATUS,
+                              default='rascunho')
+
+    class Meta:
+        ordering = ('-published',)
+
+    def __str__(self):
+        return '{} - {}'.format(self.title, self.status)
+
